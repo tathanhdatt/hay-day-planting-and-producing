df95f536f871c5ddda448f6827f0f8042759a761
Datta <ttdat.dora@gmail.com>
2025-01-08 15:36:46 +0700

    Complete camera zoom and movement

---
+++ Assets/_Game/Camera/Scripts/CameraMovement.cs
@@ -0,0 +1,141 @@
+﻿using Dt.Attribute;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraMovement : MonoBehaviour
+{
+    private const float topBoundary = 0.8f;
+    private const float bottomBoundary = 0.2f;
+    private const float leftBoundary = 0.3f;
+    private const float rightBoundary = 0.7f;
+
+    [Title("Bound")]
+    [SerializeField]
+    private float xMin;
+
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float yMin;
+
+    [SerializeField]
+    private float yMax;
+
+    [Title("")]
+    [SerializeField, Required]
+    private Camera cam;
+
+    [SerializeField]
+    private float speedInEditing;
+
+    [SerializeField, ReadOnly]
+    private Vector3 lastTouchPos;
+
+    [SerializeField, ReadOnly]
+    private bool canMove;
+
+    private Touch touch;
+
+    private void Update()
+    {
+        if (GameState.isEditing)
+        {
+            this.canMove = false;
+            MoveCameraWhileEditing();
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void MoveCameraWhileEditing()
+    {
+        if (!HasTouch()) return;
+        this.touch = Input.GetTouch(0);
+        if (!IsClosedDeviceBoundary()) return;
+        Vector3 touchWorldPos = this.cam.ScreenToWorldPoint(this.touch.position);
+        Vector3 pos = Vector3.Lerp(
+            this.cam.transform.position,
+            touchWorldPos,
+            Time.deltaTime * this.speedInEditing);
+        this.cam.transform.position = ClampCameraPosition(pos);
+    }
+
+    private bool IsClosedDeviceBoundary()
+    {
+        Vector3 viewportPoint = this.cam.ScreenToViewportPoint(this.touch.position);
+        bool isInHorizontalBoundary = viewportPoint.x is < leftBoundary or > rightBoundary;
+        bool isInVerticalBoundary = viewportPoint.y is < bottomBoundary or > topBoundary;
+        return isInHorizontalBoundary || isInVerticalBoundary;
+    }
+
+    private void Move()
+    {
+        bool isZooming = Input.touchCount == CameraZoom.NumberOfZoomFingers;
+        if (!HasTouch() || isZooming) return;
+        this.touch = Input.GetTouch(0);
+        HandleByTouchPhase();
+    }
+
+    private bool HasTouch()
+    {
+        return Input.touchCount > 0;
+    }
+
+    private void HandleByTouchPhase()
+    {
+        switch (this.touch.phase)
+        {
+            case TouchPhase.Began:
+                OnTouchBegan();
+                break;
+            case TouchPhase.Moved:
+                OnTouchMoved();
+                break;
+            case TouchPhase.Ended:
+                OnTouchEnded();
+                break;
+        }
+    }
+
+    private void OnTouchBegan()
+    {
+        bool isOverUI = EventSystem.current.IsPointerOverGameObject();
+        if (isOverUI) return;
+        this.canMove = true;
+        UpdateLastTouchPos();
+    }
+
+    private void UpdateLastTouchPos()
+    {
+        this.lastTouchPos = this.cam.ScreenToWorldPoint(this.touch.position);
+    }
+
+    private void OnTouchMoved()
+    {
+        if (!this.canMove) return;
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector3 direction = this.lastTouchPos - this.cam.ScreenToWorldPoint(this.touch.position);
+        Vector3 position = this.cam.transform.position + direction;
+        this.cam.transform.position = ClampCameraPosition(position);
+    }
+
+    private Vector3 ClampCameraPosition(Vector3 position)
+    {
+        position.x = Mathf.Clamp(position.x, this.xMin, this.xMax);
+        position.y = Mathf.Clamp(position.y, this.yMin, this.yMax);
+        position.z = -10;
+        return position;
+    }
+
+    private void OnTouchEnded()
+    {
+        this.canMove = false;
+    }
+}
---
+++ Assets/_Game/Camera/Scripts/CameraZoom.cs
@@ -0,0 +1,90 @@
+﻿using Dt.Attribute;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public const int NumberOfZoomFingers = 2;
+    [SerializeField, Required]
+    private Camera cam;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float minZoom;
+
+    [SerializeField]
+    private float maxZoom;
+
+    [SerializeField]
+    private float alignSpeed;
+
+    [SerializeField, ReadOnly]
+    private Vector3 offset;
+
+    [SerializeField, ReadOnly]
+    private Vector3 pointBetweenTwoTouch;
+
+    [SerializeField, ReadOnly]
+    private float lastSqrtMagnitude;
+
+    [SerializeField, Required]
+    private Transform pivot;
+
+    private void Update()
+    {
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        if (GameState.isEditing) return;
+        bool hasTwoFinger = Input.touchCount == NumberOfZoomFingers;
+        if (!hasTwoFinger) return;
+        AdjustCameraOrthographicSize();
+        // AlignCamera();
+    }
+
+    private void AdjustCameraOrthographicSize()
+    {
+        Touch firstTouch = Input.GetTouch(0);
+        Touch secondTouch = Input.GetTouch(1);
+        bool isFirstTouchMoved = firstTouch.phase == TouchPhase.Moved;
+        bool isSecondTouchMoved = secondTouch.phase == TouchPhase.Moved;
+        if (!isFirstTouchMoved && !isSecondTouchMoved) return;
+        this.cam.orthographicSize = CalcOrthographicSize(firstTouch, secondTouch);
+    }
+
+    private float CalcOrthographicSize(Touch firstTouch, Touch secondTouch)
+    {
+        float sqrtMagnitude = (firstTouch.position - secondTouch.position).sqrMagnitude;
+        float sign = Mathf.Sign(this.lastSqrtMagnitude - sqrtMagnitude);
+        float size = this.cam.orthographicSize + sign * this.speed * Time.deltaTime;
+        size = Mathf.Clamp(size, this.minZoom, this.maxZoom);
+        this.lastSqrtMagnitude = sqrtMagnitude;
+        return size;
+    }
+
+    private void AlignCamera()
+    {
+        Touch firstTouch = Input.GetTouch(0);
+        Touch secondTouch = Input.GetTouch(1);
+        CalcOffset(firstTouch, secondTouch);
+        Vector3 pos = Vector3.Lerp(
+            this.cam.transform.position + this.offset,
+            this.pointBetweenTwoTouch,
+            this.alignSpeed * Time.deltaTime);
+        pos -= this.offset;
+        pos.z = this.cam.transform.position.z;
+        this.cam.transform.position = pos;
+    }
+
+    private void CalcOffset(Touch firstTouch, Touch secondTouch)
+    {
+        Vector3 firstWorldPos = this.cam.ScreenToWorldPoint(firstTouch.position);
+        Vector3 secondWorldPos = this.cam.ScreenToWorldPoint(secondTouch.position);
+        this.pointBetweenTwoTouch = Vector3.Lerp(firstWorldPos, secondWorldPos, 0.5f);
+        this.offset = this.pointBetweenTwoTouch - this.cam.transform.position;
+        this.pivot.position = this.pointBetweenTwoTouch;
+    }
+}
