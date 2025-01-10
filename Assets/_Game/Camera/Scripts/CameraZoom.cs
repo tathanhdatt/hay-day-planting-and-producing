b7cbdd6828131025a8ff4dca5337e43c2c881f4c
Datta <ttdat.dora@gmail.com>
2025-01-10 15:00:38 +0700

    Update zoom camera to point between 2 fingers

--- Assets/_Game/Camera/Scripts/CameraZoom.cs
+++ Assets/_Game/Camera/Scripts/CameraZoom.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
 {
-    public const int NumberOfZoomFingers = 2;
+    private const int numberOfZoomFingers = 2;
+
     [SerializeField, Required]
     private Camera cam;
 
@@ -18,20 +19,11 @@ public class CameraZoom : MonoBehaviour
     [SerializeField]
     private float maxZoom;
 
-    [SerializeField]
-    private float alignSpeed;
-
-    [SerializeField, ReadOnly]
-    private Vector3 offset;
-
-    [SerializeField, ReadOnly]
-    private Vector3 pointBetweenTwoTouch;
-
     [SerializeField, ReadOnly]
     private float lastSqrtMagnitude;
 
-    [SerializeField, Required]
-    private Transform pivot;
+    [SerializeField, ReadOnly]
+    private Vector3 beginZoomWorldPos;
 
     private void Awake()
     {
@@ -46,9 +38,36 @@ public class CameraZoom : MonoBehaviour
     private void OnGestureHandler(List<LeanFinger> fingers)
     {
         if (GameState.isEditing) return;
-        if (fingers.Count != NumberOfZoomFingers) return;
+        if (fingers.Count != numberOfZoomFingers) return;
+        if (IsAnyFingerOverGUI(fingers)) return;
+        if (fingers[0].Down || fingers[1].Down)
+        {
+            OnBeginZoom(fingers[0], fingers[1]);
+        }
+
         if (!IsAnyFingerMoved(fingers)) return;
         AdjustOrthographicSize(fingers[0], fingers[1]);
+        AlignCamera(fingers[0].ScreenPosition, fingers[1].ScreenPosition);
+        
+    }
+
+    private void OnBeginZoom(LeanFinger first, LeanFinger second)
+    {
+        Vector2 beginZoomScreenPos = Vector2.Lerp(first.ScreenPosition, second.ScreenPosition, 0.5f);
+        this.beginZoomWorldPos = this.cam.ScreenToWorldPoint(beginZoomScreenPos);
+    }
+
+    private bool IsAnyFingerOverGUI(List<LeanFinger> fingers)
+    {
+        foreach (LeanFinger finger in fingers)
+        {
+            if (finger.IsOverGui)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private bool IsAnyFingerMoved(List<LeanFinger> fingers)
@@ -60,7 +79,7 @@ public class CameraZoom : MonoBehaviour
                 return true;
             }
         }
-        
+
         return false;
     }
 
@@ -79,6 +98,10 @@ public class CameraZoom : MonoBehaviour
     private float CalcOrthographicSize(Vector2 firstFingerPos, Vector2 secondFingerPos)
     {
         float sqrtMagnitude = (firstFingerPos - secondFingerPos).sqrMagnitude;
+        if (Mathf.Approximately(sqrtMagnitude, this.lastSqrtMagnitude))
+        {
+            return this.cam.orthographicSize;
+        }
         float sign = Mathf.Sign(this.lastSqrtMagnitude - sqrtMagnitude);
         float size = this.cam.orthographicSize + sign * this.speed * Time.deltaTime;
         size = Mathf.Clamp(size, this.minZoom, this.maxZoom);
@@ -86,27 +109,12 @@ public class CameraZoom : MonoBehaviour
         return size;
     }
 
-    private void AlignCamera()
+    private void AlignCamera(Vector2 first, Vector2 second)
     {
-        Touch firstTouch = Input.GetTouch(0);
-        Touch secondTouch = Input.GetTouch(1);
-        CalcOffset(firstTouch, secondTouch);
-        Vector3 pos = Vector3.Lerp(
-            this.cam.transform.position + this.offset,
-            this.pointBetweenTwoTouch,
-            this.alignSpeed * Time.deltaTime);
-        pos -= this.offset;
-        pos.z = this.cam.transform.position.z;
-        this.cam.transform.position = pos;
-    }
-
-    private void CalcOffset(Touch firstTouch, Touch secondTouch)
-    {
-        Vector3 firstWorldPos = this.cam.ScreenToWorldPoint(firstTouch.position);
-        Vector3 secondWorldPos = this.cam.ScreenToWorldPoint(secondTouch.position);
-        this.pointBetweenTwoTouch = Vector3.Lerp(firstWorldPos, secondWorldPos, 0.5f);
-        this.offset = this.pointBetweenTwoTouch - this.cam.transform.position;
-        this.pivot.position = this.pointBetweenTwoTouch;
+        Vector2 centerScreenPoint = Vector2.Lerp(first, second, 0.5f);
+        Vector3 centerWorldPos = this.cam.ScreenToWorldPoint(centerScreenPoint);
+        Vector3 worldOffset = centerWorldPos - this.cam.transform.position;
+        this.cam.transform.position = this.beginZoomWorldPos - worldOffset;
     }
 
     private void OnDestroy()
