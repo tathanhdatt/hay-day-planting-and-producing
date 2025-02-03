f4983772e45ea4203dea7188d16116bf8b05074d
Datta <ttdat.dora@gmail.com>
2025-02-03 10:50:13 +0700

    Remove enable collider in scripts

--- Assets/_Game/Tooltips/Scripts/ProducedSlotDetector.cs
+++ Assets/_Game/Tooltips/Scripts/ProducedSlotDetector.cs
@@ -5,46 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class ProducedSlotDetector : MonoBehaviour
 {
-    [SerializeField, Required]
-    private UIDrag drag;
-
-    [SerializeField, Required]
-    private Collider2D detectSlotCollider;
-
     [SerializeField, ReadOnly]
     private ProducedSlot lastProducedSlot;
 
     [SerializeField, ReadOnly]
     private Collider2D lastCollider2D;
-    
+
     public event Action<ProducedSlot> OnDetectedSlot;
-
-    public void Initialize()
-    {
-        this.detectSlotCollider.enabled = false;
-        this.drag.OnFingerDown += OnFingerDownHandler;
-        this.drag.OnFingerUp += OnFingerUpHandler;
-    }
-
-    private void OnFingerDownHandler()
-    {
-        this.detectSlotCollider.enabled = true;
-    }
-
-    private void OnFingerUpHandler()
-    {
-        this.detectSlotCollider.enabled = false;
-    }
-
-    private void OnEnable()
-    {
-        this.detectSlotCollider.enabled = true;
-    }
-
-    private void OnDisable()
-    {
-        this.detectSlotCollider.enabled = false;
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
